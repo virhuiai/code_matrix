09e71e429eaae90289fee1e230763c6e1c182c4a
virhuiai <>
2025-04-19 00:09:48 +0800

    CshWebSocket

---
+++ code/java_virhuiai/CshWebSocket/src/main/java/com/virhuiai/CshWebSocket/App.java
@@ -0,0 +1,21 @@
+package com.virhuiai.CshWebSocket;
+
+
+
+import com.virhuiai.CshLogUtils.CshLogUtils;
+import org.apache.commons.logging.Log;
+
+//
+/*
+ln -s /Volumes/RamDisk/tmp/ /Volumes/THAWSPACE/CshProject/code_matrix.git/code/java_virhuiai/CshCdtJavaClient/target
+ln -s /Volumes/RamDisk/tmp/ /Volumes/THAWSPACE/CshProject/code_matrix.git/code/java_virhuiai/CshCdtJavaClient/logs
+*/
+public class App {
+    private static final Log LOGGER = CshLogUtils.createLogExtended(App.class); // 日志记录器
+
+    public static void main(String[] args) {
+        System.out.println("s");
+
+    }
+
+}
