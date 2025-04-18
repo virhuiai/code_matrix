3b20a57ed3d2d368204aa5c95dcac2bf5b6b17b7
virhuiai <>
2025-04-18 23:17:16 +0800

    BlockUrlsExample

---
+++ code/java_virhuiai/CshCdtJavaClient/src/main/java/com/virhuiai/CshCdtJavaClient/BlockUrlsExample.java
@@ -0,0 +1,71 @@
+package com.virhuiai.CshCdtJavaClient;
+
+/*-
+ * #%L
+ * cdt-examples
+ * %%
+ * Copyright (C) 2018 - 2021 Kenan Klisura
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import com.github.kklisura.cdt.launch.ChromeLauncher;
+import com.github.kklisura.cdt.protocol.commands.Network;
+import com.github.kklisura.cdt.protocol.commands.Page;
+import com.github.kklisura.cdt.services.ChromeDevToolsService;
+import com.github.kklisura.cdt.services.ChromeService;
+import com.github.kklisura.cdt.services.types.ChromeTab;
+import java.util.Arrays;
+
+/**
+ * Blocks specific urls from fetching. In this case png and css.
+ *
+ * @author Kenan Klisura
+ */
+public class BlockUrlsExample {
+  public static void main(String[] args) {
+    // Create chrome launcher.
+    final ChromeLauncher launcher = new ChromeLauncher();
+
+    // Launch chrome either as headless (true) or regular (false).
+    final ChromeService chromeService = launcher.launch(false);
+
+    // Create empty tab ie about:blank.
+    final ChromeTab tab = chromeService.createTab();
+
+    // Get DevTools service to this tab
+    final ChromeDevToolsService devToolsService = chromeService.createDevToolsService(tab);
+
+    // Get individual commands
+    final Page page = devToolsService.getPage();
+    final Network network = devToolsService.getNetwork();
+
+    network.setBlockedURLs(Arrays.asList("*.png", "*.css"));
+    page.onLoadEventFired(event -> devToolsService.close());
+
+    network.enable();
+
+    // Enable page events.
+    page.enable();
+
+    // Navigate to github.com.
+    page.navigate("http://github.com");
+
+    devToolsService.waitUntilClosed();
+  }
+
+  public static boolean isBlocked(String url) {
+    return url.endsWith(".png") || url.endsWith(".css");
+  }
+}
