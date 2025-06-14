ce45fc5c4db30800c403ece4dd7bc07e054e4cd5
virhuiai <>
2025-06-14 20:09:46 +0800

    自定义CEF资源处理器

---
+++ code/java_virhuiai/1_CshLog/src/main/java/com/virhuiai/CshLogUtils/v2/Tmp.java
@@ -0,0 +1,11 @@
+package com.virhuiai.CshLogUtils.v2;
+
+import com.virhuiai.CshLogUtils.v2.LogHelper;
+import org.apache.commons.logging.Log;
+
+public class Tmp {
+    public static void main(String[] args) {
+        Log logger = LogHelper.getLogger();
+        logger.debug("abc");
+    }
+}
