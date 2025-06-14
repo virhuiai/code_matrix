870eb44c4d28f07bab7e23a9996426f809db15e6
virhuiai <>
2025-06-14 21:44:57 +0800

    .

---
+++ code/java_virhuiai/org.apache.commons.lang-3.17.0/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        StringUtils.isNotBlank()
+        System.out.println("Hello world!");
+    }
+}
