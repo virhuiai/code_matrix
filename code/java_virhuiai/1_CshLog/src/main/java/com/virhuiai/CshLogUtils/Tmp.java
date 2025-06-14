c227e0a95b123bac0fee50af25489ea61aee477b
virhuiai <>
2025-06-14 16:44:56 +0800

    LogHelper

---
+++ code/java_virhuiai/1_CshLog/src/main/java/com/virhuiai/CshLogUtils/Tmp.java
@@ -0,0 +1,10 @@
+package com.virhuiai.CshLogUtils;
+
+import org.apache.commons.logging.Log;
+
+public class Tmp {
+    public static void main(String[] args) {
+        Log logger = LogHelper.getLogger();
+        logger.debug("abc");
+    }
+}
