ef9135f4602617200c3a467cad5d6c8cee2e0bc3
virhuiai <>
2025-06-15 00:57:21 +0800

    1. 将字符串按指定分隔符分割成字符串列表: 2. 将字符串按空白字符分割成字符串数组: 3. 将字符串按指定长度分割成字符串列表: 4. 将驼峰式命名的字符串分割成单词列表:

--- code/java_virhuiai/CshString/src/main/java/com/virhuiai/string/StringSplitUtils.java
+++ code/java_virhuiai/CshString/src/main/java/com/virhuiai/string/StringSplitUtils.java
@@ -1,6 +1,7 @@
 package com.virhuiai.string;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -50,4 +51,76 @@ public interface StringSplitUtils {
         }
         return tokenizeToStringArray(str, delimiters, true, true);
     }
+
+    /**
+     * 将字符串按指定分隔符分割成字符串列表
+     * @param str 要分割的字符串
+     * @param delimiter 分隔符
+     * @return 分割后的字符串列表,如果原字符串为空,返回空列表
+     */
+    default List<String> split(String str, String delimiter) {
+        if (str == null) {
+            return new ArrayList<>();
+        }
+        String[] tokens = str.split(delimiter);
+        return Arrays.asList(tokens);
+    }
+
+    /**
+     * 将字符串按空白字符分割成字符串数组
+     * @param str 要分割的字符串
+     * @return 分割后的字符串数组,如果原字符串为空,返回空数组
+     */
+    default String[] splitByWhitespace(String str) {
+        if (str == null) {
+            return new String[0];
+        }
+        return str.split("\\s+");
+    }
+
+    /**
+     * 将字符串按指定长度分割成字符串列表
+     * @param str 要分割的字符串
+     * @param length 分割后每个字符串的长度
+     * @return 分割后的字符串列表,如果原字符串为空,返回空列表
+     */
+    default List<String> splitByLength(String str, int length) {
+        if (str == null) {
+            return new ArrayList<>();
+        }
+        List<String> list = new ArrayList<>();
+        int index = 0;
+        while (index < str.length()) {
+            list.add(str.substring(index, Math.min(index + length, str.length())));
+            index += length;
+        }
+        return list;
+    }
+
+    /**
+     * 将驼峰式命名的字符串分割成单词列表
+     * @param str 要分割的字符串
+     * @return 分割后的单词列表,如果原字符串为空,返回空列表
+     */
+    default List<String> splitCamelCase(String str) {
+        if (str == null) {
+            return new ArrayList<>();
+        }
+        List<String> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        for (char c : str.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                if (sb.length() > 0) {
+                    list.add(sb.toString());
+                    sb.setLength(0);
+                }
+            }
+            sb.append(Character.toLowerCase(c));
+        }
+        if (sb.length() > 0) {
+            list.add(sb.toString());
+        }
+        return list;
+    }
+
 }
