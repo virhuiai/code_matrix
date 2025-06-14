f3c94f00639b14e8c7ecbfe51890cb63d8332a7c
virhuiai <>
2025-06-15 01:37:51 +0800

    包含获取字符序列长度的方法+单元测试

--- code/java_virhuiai/CshString/src/main/java/com/virhuiai/string/StringLengthUtils.java
+++ code/java_virhuiai/CshString/src/main/java/com/virhuiai/string/StringLengthUtils.java
@@ -14,4 +14,24 @@ public interface StringLengthUtils {
     default int length(CharSequence cs) {
         return cs == null ? 0 : cs.length();
     }
+
+    /**
+     * 获取字符序列的实际长度(去除前后空白字符)
+     * @param cs 字符序列
+     * @return 如果字符序列为null,返回0,否则返回去除前后空白字符后的实际长度
+     */
+    default int lengthWithoutWhitespace(CharSequence cs) {
+        if (Str.Utils.isBlank(cs)) {
+            return 0;
+        }
+        int start = 0;
+        int end = cs.length();
+        while (start < end && Character.isWhitespace(cs.charAt(start))) {
+            start++;
+        }
+        while (end > start && Character.isWhitespace(cs.charAt(end - 1))) {
+            end--;
+        }
+        return end - start;
+    }
 }
