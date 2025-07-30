e80f458cf363f86956c53c3a7e1cc99785c005d0
virhuiai <>
2025-07-30 23:56:10 +0800

    注释

--- code/java_virhuiai/matrix/csh-string/src/main/java/com/virhuiai/log/string/Str.java
+++ code/java_virhuiai/matrix/csh-string/src/main/java/com/virhuiai/log/string/Str.java
@@ -1,38 +1,36 @@
 package com.virhuiai.log.string;
 
-
 /**
- * 字符串工具类
-
- StringValidationUtils
- StringTransformationUtils
- StringSplitUtils
- StringDesensitizeUtils
- StringSubstringUtils
- StringCompareUtils
- ObjectStringUtils
- StringLengthUtils
-
- StringUtils类  具有了这些接口定义的所有方法。
-
- *
- *
- * 使用枚举实现单例模式有几个好处:
- *
- * 它保证了单例的唯一性。每个枚举类型只能有一个实例。
- * 它是线程安全的。枚举常量的创建是线程安全的。
- * 它可以防止反序列化重新创建新的对象。枚举类型默认实现了 Serializable 接口,并确保反序列化时返回同一个实例。
- *
- <groupId>virhuiai</groupId>
- <artifactId>CshString</artifactId>
- <version>1.1</version>
-
+ * 字符串工具类，实现了多个字符串操作相关接口，具备字符串验证、转换、分割、脱敏等功能。
+ * 该类使用枚举实现单例模式，确保全局只有一个实例。
+ * 
+ * 涉及的接口及对应功能如下：
+ * - StringValidationUtils：字符串验证
+ * - StringTransformationUtils：字符串转换
+ * - StringSplitUtils：字符串分割
+ * - StringDesensitizeUtils：字符串脱敏
+ * - StringSubstringUtils：字符串截取
+ * - StringCompareUtils：字符串比较
+ * - ObjectStringUtils：对象转字符串
+ * - StringLengthUtils：字符串长度处理
+ * 
+ * 该类实现了上述接口定义的所有方法。
+ * 
+ * 使用枚举实现单例模式的优势：
+ * - 唯一性保证：每个枚举类型只能有一个实例，确保单例的唯一性。
+ * - 线程安全：枚举常量的创建过程是线程安全的。
+ * - 防止反序列化问题：枚举类型默认实现了 Serializable 接口，能确保反序列化时返回同一个实例。
+ * 
+ * 项目信息：
+<groupId>virhuiai</groupId>
+<artifactId>CshString</artifactId>
+<version>1.1</version>
  */
 public enum Str
         implements Validation, Transformation,
         Split, Desensitize,
         Substring, Compare,
-        ToString, ToJson,ToStringDepthLimited,ToStringnCycleDetection,
+        ToString, ToJson, ToStringDepthLimited, ToStringnCycleDetection,
         Length, Url
 {
     Utils;
